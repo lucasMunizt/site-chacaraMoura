01b090563524f7256a6e654f7b107e9f5fdebad5
lucasMunizt <lucasmunizteles@gmail.com>
2025-12-17 10:23:28 -0300

    adicionnado alteração

--- site/src/plataforma/home.tsx
+++ site/src/plataforma/home.tsx
@@ -4,9 +4,10 @@ const Home = () => {
   return (
     <div>
       <header className="p-1 h-20 bg-[#0b0f19]">
-        {/* <h4 className="text-white">Plataforma - Chácara Moura</h4> */}
-        <img src="/public/logo-menor.png" alt="" className="w-16"/>
+        <img src="/public/logo-menor.png" alt="logo escrita cm" className="w-16 m-auto mt-1.5"/>
       </header>
+        
+
       <Header />
     </div>
   );
