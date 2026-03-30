5173f0149f31da562feaa6cfdd2c50f124aa6a25
lucasMunizt <lucasmunizteles@gmail.com>
2026-03-29 22:57:41 -0300

    atualização do site

--- site/src/components/Common/cardComponent.tsx
+++ site/src/components/Common/cardComponent.tsx
@@ -2,26 +2,26 @@ import { Card, CardContent } from "../ui/card";
 
 const produtos = [
   {
-    title: "Chácaras Moura 3 - bloco 2",
-    imgSrc: "/img-1.png",
-    description: "Chácaras - Tamanho padrão 10 x 50 Parcelas aparti de 195",
+    title: " Chácaras Moura - 18 ",
+    imgSrc: "/propaganda.jpeg",
+    description: "Parcelas apartir de R$: 390,00",
   },
 
   {
-    title: "Chácaras Moura 13 - Ramal do Coco 2",
-    imgSrc: "/img-2.png",
-    description: "Chácaras - Tamanho padrão 10 x 50 Parcelas aparti de 195",
+    title: "Chácaras Moura 17 - cipó Amber",
+    imgSrc: "/moura-17-1.jpeg",
+    description: "Ganhe 20 mudas de cacau",
   },
   {
-    title: "Chácaras Moura 15 - bloco 2",
-    imgSrc: "/img-3.png",
-    description: "Chácaras - Tamanho padrão 10 x 50 Parcelas aparti de 195",
+    title: "Moura 17 - Cipó Amber - BLOCO 2",
+    imgSrc: "/moura-17-2.jpeg",
+    // description: "Chácaras - Tamanho padrão 10 x 50 Parcelas aparti de 195",
   },
-  {
-    title: "Chácaras Moura 16",
-    imgSrc: "/img-4.png",
-    description: "Chácaras - Tamanho padrão 10 x 50 Parcelas aparti de 195",
-  },
+  // {
+  //   title: "Chácaras Moura 16",
+  //   imgSrc: "/img-4.png",
+  //   // description: "Chácaras - Tamanho padrão 10 x 50 Parcelas aparti de 195",
+  // },
 ];
 
 export const CardComponent = () => {
@@ -36,28 +36,34 @@ export const CardComponent = () => {
             Descubra os benefícios de ter seu próprio refúgio no campo
           </p>
         </div>
-
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
-          {produtos.map((produto, index) => {
-            return (
-              <Card
-                key={index}
-                className="group hover:shadow-lg transition-all duration-300 hover:-translate-y-1 border-border bg-card "
-                style={{ animationDelay: `${index * 100}ms` }}
-              >
-                <CardContent className="p-6 text-center">
-                  <img
-                    src={produto.imgSrc}
-                    className=""
-                  />
-                  <h3 className="text-xl font-semibold mb-3 text-card-foreground">
-                    {produto.title}
-                  </h3>
-                  <p className="text-muted-foreground relative top-3.5">{produto.description}</p>
-                </CardContent>
-              </Card>
-            );
-          })}
+        <div className="flex justify-center">
+          <div
+            className="grid grid-cols-1 mx-auto w-full max-w-6xl
+         md:grid-cols-2 lg:grid-cols-3 gap-8 items-center "
+          >
+            {produtos.map((produto, index) => {
+              return (
+                <Card
+                  key={index}
+                  className="group hover:shadow-lg transition-all duration-300 hover:-translate-y-1 border-border bg-card "
+                  style={{ animationDelay: `${index * 100}ms` }}
+                >
+                  <CardContent className="p-6 text-center">
+                    <img
+                      src={produto.imgSrc}
+                      className=" h-auto flex items-center justify-center m-auto max-h-50 mb-2 w-full rounded-md"
+                    />
+                    <h3 className="text-xl font-semibold mb-3 text-card-foreground">
+                      {produto.title}
+                    </h3>
+                    <p className="text-muted-foreground relative top-3.5">
+                      {produto.description}
+                    </p>
+                  </CardContent>
+                </Card>
+              );
+            })}
+          </div>
         </div>
       </div>
     </section>
